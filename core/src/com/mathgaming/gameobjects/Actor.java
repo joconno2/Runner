e935d756519d7dabb1a281dbb909283ba1e9d488
Jim O'Connor <joconno2@conncoll.edu>
2014-09-18 13:43:53 -0400

    Created Actor
    Created the basic actor class to be used by the main character of the
    runner. Only basic methods implemented thus far.

---
+++ core/src/com/mathgaming/gameobjects/Actor.java
@@ -0,0 +1,51 @@
+package com.mathgaming.gameobjects;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Actor {
+
+	private Vector2 position;
+    private Vector2 velocity;
+    private Vector2 acceleration;
+
+    private int width;
+    private int height;
+    
+    public Actor(float x, float y, int width, int height){
+    	this.width = width;
+        this.height = height;
+        position = new Vector2(x, y);
+        velocity = new Vector2(0, 0);
+        acceleration = new Vector2(0, 460);
+    }
+    
+    public void update(float delta){
+    	velocity.add(acceleration.cpy().scl(delta));
+
+        if (velocity.y > 200) {
+            velocity.y = 200;
+        }
+
+        position.add(velocity.cpy().scl(delta));
+    }
+    
+    public void jump(){
+    	velocity.y = -100;
+    }
+	
+    public float getX() {
+        return position.x;
+    }
+
+    public float getY() {
+        return position.y;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+}
