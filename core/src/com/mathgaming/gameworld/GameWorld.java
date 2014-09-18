0476e5979b7c8cc446fe7cb20e262af50c14a166
Jim O'Connor <joconno2@conncoll.edu>
2014-09-18 16:11:23 -0400

    Apparently Actor is a reserved word in libgdx
    fixed that, its RunActor now

--- core/src/com/mathgaming/gameworld/GameWorld.java
+++ core/src/com/mathgaming/gameworld/GameWorld.java
@@ -1,28 +1,20 @@
 package com.mathgaming.gameworld;
 
-import java.util.Random;
-
-import com.badlogic.gdx.math.Rectangle;
+import com.mathgaming.gameobjects.RunActor;
 
 public class GameWorld {
-
-	private Rectangle rect = new Rectangle(40, 20, 17, 12);
-	private Random r = new Random();
+	private RunActor actor;
+	
+	public GameWorld(int midpointY){
+		actor = new RunActor(40.0f,140.0f,20,20);
+	}
 
     public void update(float delta) {
-    	
-    	rect.x--;
-        if (rect.x < -17) {
-            rect.x = 136;
-            rect.y = r.nextInt(200);
-        }
-        
-        
-        
+    	actor.update(delta);
     }
 
-    public Rectangle getRect() {
-        return rect;
+    public RunActor getRunActor() {
+        return actor;
     }
 
 }
